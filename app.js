08b20f881cdbf973f4dbf315e8492f0a81a101b4
melarKode <58908326+melarKode@users.noreply.github.com>
2020-04-07 14:00:46 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const fs = require('fs');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+//EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+//Body Parser
+app.use(express.urlencoded({extended:false}));
+
+//Static
+app.use(express.static('views'));
+
+//Routes
+app.use('/', require('./routes/index'));
+
+app.listen(PORT, ()=>console.log(`Listening to port ${PORT}`));
