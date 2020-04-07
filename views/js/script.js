453c3202e6e212a8aaf828ae61aa411b6b3666da
Navneet <navneet.melarkode@gmail.com>
2020-04-07 16:35:48 +0530

    Homepage access to dictionary

--- views/js/script.js
+++
@@ -1,7 +0,0 @@
-const word = Qs.parse(location.search,{
-    ignoreQueryPrefix: true
-});
-const w = word.search;
-console.log(word.search);
-const wordPrint = document.getElementById('word');
-wordPrint.innerHTML = word.search;
