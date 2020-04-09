9578fd45bb9feb53030f425a49583e1cd6835399
Navneet <navneet.melarkode@gmail.com>
2020-04-09 09:19:19 +0530

    Error handling

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const fs = require('fs');
+const ngrok = require('ngrok');
+const flash = require('connect-flash');
 
 const PORT = process.env.PORT || 3000;
 
@@ -16,8 +18,15 @@ app.use(express.urlencoded({extended:false}));
 //Static
 app.use(express.static('views'));
 
+//Connect Flash
+app.use(flash());
+
 //Routes
 app.use('/', require('./routes/index'));
 app.use('/word', require('./routes/word'));
 
 app.listen(PORT, ()=>console.log(`Listening to port ${PORT}`));
+ngrok.connect(PORT)
+    .then(url=>{
+        console.log(url);
+    })
--- routes/word.js
+++ routes/word.js
@@ -1,16 +1,46 @@
 const express = require('express');
 const fs = require('fs');
 const router = express.Router();
-const datapath = './data/dictionary.json';
+var test = './data/testing/D';
 
 router.get('/:word',(req,res)=>{
-    var dict = JSON.parse(fs.readFileSync(datapath));
-    var word = req.params.word.toLowerCase();
-    if(dict[word])
-        var def = dict[word];
-    else
-        var def = "Incorrect word entered";
-    res.render('testing', {word:word, def:def});
+    var errors = []; 
+    var word = req.params.word.toUpperCase();
+    var letter = word[0].toUpperCase();
+    if(letter.toLowerCase() == letter.toUpperCase())
+    {
+        errors.push({msg: `No results found for ${req.params.word}`});
+    }
+    if(errors.length>0)
+    {
+        res.render('definition', {errors : errors[0], word:'', syno: [], anto: [], def: ''});
+    }else{
+        test = test+ letter+'.json';
+        var dict =JSON.parse(fs.readFileSync(test));
+        test = './data/testing/D';
+        var meanings = [];
+        var antonyms = [];
+        var synonyms = [];
+        if(dict[word])
+        {
+            for(var obj in dict[word]["MEANINGS"])
+            meanings.push(dict[word]["MEANINGS"][obj]);
+            antonyms.push(dict[word]["ANTONYMS"]);
+            antonyms = antonyms[0];
+            synonyms.push(dict[word]["SYNONYMS"]);  
+            synonyms = synonyms[0];
+        }
+        else
+        {
+            errors.push({msg: `No results found for ${req.params.word}`});
+        }
+        if(errors.length>0)
+        {
+            res.render('definition', {errors: errors[0], word:'', syno: ['N/A'], anto: ['N/A'], def:''});
+        }else{
+            res.render('definition', {word:word, def:meanings, anto:antonyms, syno: synonyms});
+        }
+    }
 });
 
 module.exports = router;
